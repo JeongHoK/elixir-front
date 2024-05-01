32707c1414ad120d974362b55d50fabebef9cf61
jeongho <wjdgh9531@gmail.com>
2024-05-01 17:21:46 +0900

    Upd: 엘릭서 화면 및 backend 연동 완료

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
+import axios from 'axios';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.config.globalProperties.axios = axios
+app.config.globalProperties.HOST = "http://localhost:8000"
+app.mount('#app');
--- src/views/Elixir.vue
+++ src/views/Elixir.vue
@@ -1,21 +1,21 @@
 <template>
-    <div style="display: flex; width:1200px; justify-content: space-evenly; margin-top: 15px;">
+    <div style="display: flex; width:750px; margin-top: 15px;">
         <div>
-            <div>
+            <div style="width: 200px; margin-left: 30px;">
                 <div style="margin-bottom: 5px;">전체 연성 횟수 입력</div>
                 <div style="margin-bottom: 5px;">
-                    <input type="text" style="width: 80px; margin-right: 10px;">
-                    <button type="button" class="btn btn-primary">확인</button>
+                    <input type="number" style="width: 80px; margin-right: 10px;" v-model="inputCount" v-bind:disabled="inputDisabled">
+                    <button type="button" class="btn btn-primary" @click="inputCountOk">확인</button>
                 </div>
                 <div style="text-align: center;">
-                    <button type="button" class="btn btn-primary">초기화</button>
+                    <button type="button" class="btn btn-primary" @click="resetElixir">초기화</button>
                 </div>
             </div>
-            <div>
-                <table class="table">
+            <div style="text-align: center;">
+                <table class="table table-bordered table-custom">
                     <tbody>
-                        <tr v-for="(i) in list" v-bind:key="i">
-                            <td>{{ i }}</td>
+                        <tr v-for="(i, index) in list" v-bind:key="index">
+                            <td :class="{'bg-custom' : index === this.elixirDto.ductilityCount }">{{ i }}</td>
                         </tr>
                     </tbody>
                 </table>
@@ -23,44 +23,44 @@
         </div>
 
         <div>
-            <div style="width: 230px; height: 110px;">rebedo</div>
-            <table class="table">
+            <div style="width: 100px; height: 110px;">rebedo</div>
+            <table class="table table-bordered table-custom">
                 <tbody>
-                    <tr v-for="(i) in list" v-bind:key="i">
-                        <td>{{ i }}</td>
+                    <tr v-for="(i, index) in elixirDto.wisePersons.rebedo" v-bind:key="index">
+                        <td :class="{'bg-order-custom' : i === 3 && elixirDto.selectWisePerson === 'rebedo', 'bg-chaos-custom' : i === 6 && elixirDto.selectWisePerson !== 'rebedo', 'bg-custom' : index === this.elixirDto.ductilityCount}">{{ i }}</td>
                     </tr>
                 </tbody>
             </table>
             <div style="text-align: center;">
-                <button type="button" class="btn btn-primary" style="width: 80px;">선택</button>
+                <button type="button" class="btn btn-primary" style="width: 80px;" @click="selectWisePerson('rebedo')">선택</button>
             </div>
         </div>
 
         <div>
-            <div style="width: 230px; height: 110px;">viriditas</div>
-            <table class="table">
+            <div style="width: 100px; height: 110px;">viriditas</div>
+            <table class="table table-bordered table-custom">
                 <tbody>
-                    <tr v-for="(i) in list" v-bind:key="i">
-                        <td>{{ i }}</td>
+                    <tr v-for="(i, index) in elixirDto.wisePersons.viriditas" v-bind:key="index">
+                        <td :class="{'bg-order-custom' : i === 3 && elixirDto.selectWisePerson === 'viriditas', 'bg-chaos-custom' : i === 6 && elixirDto.selectWisePerson !== 'viriditas', 'bg-custom' : index === this.elixirDto.ductilityCount}">{{ i }}</td>
                     </tr>
                 </tbody>
             </table>
             <div style="text-align: center;">
-                <button type="button" class="btn btn-primary" style="width: 80px;">선택</button>
+                <button type="button" class="btn btn-primary" style="width: 80px;" @click="selectWisePerson('viriditas')">선택</button>
             </div>
         </div>
     
         <div>
-            <div style="width: 230px; height: 110px;">citrini</div>
-            <table class="table">
+            <div style="width: 100px; height: 110px;">citrini</div>
+            <table class="table table-bordered table-custom">
                 <tbody>
-                    <tr v-for="(i) in list" v-bind:key="i">
-                        <td>{{ i }}</td>
+                    <tr v-for="(i, index) in elixirDto.wisePersons.citrini" v-bind:key="index">
+                        <td :class="{'bg-order-custom' : i === 3 && elixirDto.selectWisePerson === 'citrini', 'bg-chaos-custom' : i === 6 && elixirDto.selectWisePerson !== 'citrini', 'bg-custom' : index === this.elixirDto.ductilityCount}">{{ i }}</td>
                     </tr>
                 </tbody>
             </table>
             <div style="text-align: center;">
-                <button type="button" class="btn btn-primary" style="width: 80px;">선택</button>
+                <button type="button" class="btn btn-primary" style="width: 80px;" @click="selectWisePerson('citrini')">선택</button>
             </div>
         </div>
 
@@ -68,18 +68,87 @@
 </template>
   
   <script>
-  export default {
+export default {
     name: "VueElixir",
     props: {},
     data() {
         return {
-            list: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+            inputCount: 14,
+            list: [14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1],
+            inputDisabled: false,
+            elixirDto: {
+                wisePersons: {
+                    rebedo: new Array(14+2),
+                    viriditas: new Array(14+2),
+                    citrini: new Array(14+2)
+                },
+                selectWisePerson: null,
+                beforeSelectWisePerson: null,
+                ductilityCount: 0,
+                totalDuctilityCount: 0,
+            },
         };
-    }
+    },
+    methods: {
+        inputCountOk() {
+            this.inputDisabled = true;
+            this.list = []
+            for(let i = this.inputCount; i>-2; i--) {
+                this.list.push(i);
+            }
+
+            this.elixirDto = {
+                wisePersons: {
+                    rebedo: new Array(this.inputCount+2),
+                    viriditas: new Array(this.inputCount+2),
+                    citrini: new Array(this.inputCount+2),
+                },
+                 selectWisePerson: null,
+                beforeSelectWisePerson: null,
+                ductilityCount: 0,
+                totalDuctilityCount: this.inputCount + 2,
+            }
+            
+        },
+        resetElixir() {
+            this.inputDisabled = false;
+            this.inputCount = 14;
+            this.list = []
+            for(let i = this.inputCount; i>-2; i--) {
+                this.list.push(i);
+            }
+        },
+        selectWisePerson(wisePersonName){
+            if(this.inputDisabled) {
+                this.elixirDto.beforeSelectWisePerson = this.elixirDto.selectWisePerson;
+                this.elixirDto.selectWisePerson = wisePersonName;
+
+                this.axios.post(this.HOST+"/select", this.elixirDto)
+                .then(response => {
+                    this.elixirDto = response.data;
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+            } else {
+                alert("연성 횟수 입력 후 확인 버튼을 눌러주세요");
+            }
+        },
+        
+    },
+    
   }
   </script>
   
   <!-- Add "scoped" attribute to limit CSS to this component only -->
   <style scoped>
+  .table-custom { width: 40px; margin: auto; margin-top: 20px; margin-bottom: 20px;}
+  .table-custom tbody tr td { height: 41px;}
+
+  .table-custom .bg-custom { background-color: rgba(255, 115, 0, 0.2); }
+
+  .table-custom .bg-order-custom { background-color: rgba(0, 117, 250, 0.247); }
+
+  .table-custom .bg-chaos-custom { background-color: rgba(233, 186, 240, 0.712); }
   </style>
   
